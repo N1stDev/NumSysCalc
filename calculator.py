4e083fcd5a8b256bd33057741c0de5c4edd82fe5
N1stDev <72524716+N1stDev@users.noreply.github.com>
2021-11-06 14:48:00 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,7 @@
-# данная программа реализует перевод дробного числа из одной системы счисления в другую
-# алгоритм сначала переводит имеющееся число в десятичную систему, а затем из десятичной в новую
-
-# Перевод имеющегося числа в десятичное число
 def not1_to_d(num, not1):
     n = 0
     number_d = 0
-    if '.' in num:  # Разделение числа на две части: w_part - целая часть, f_part - дробная часть
+    if '.' in num:
         for i in num:
             if i == '.':
                 break
@@ -17,7 +13,7 @@ def not1_to_d(num, not1):
         f_part = "0"
 
     m1 = 0
-    for i in reversed(w_part):  # перевод целой части в 10-чную систему счисления
+    for i in reversed(w_part):
         if (ord(i) >= 97) and (ord(i) <= 102):
             a = ord(i) - 87
         else:
@@ -26,7 +22,7 @@ def not1_to_d(num, not1):
         m1 += 1
 
     m2 = -1
-    for i in f_part:  # перевод дробной части в 10-чную систему счисления
+    for i in f_part:
         if (ord(i) >= 97) and (ord(i) <= 102):
             a = ord(i) - 87
         else:
@@ -37,15 +33,14 @@ def not1_to_d(num, not1):
     return number_d
 
 
-# Перевод промежуточного десятичного числа в необходимую систему счисления
 def d_to_not2(num, not2):
-    w_part = int(num)  # целая часть
-    f_part = num % 1  # дробная часть
+    w_part = int(num) 
+    f_part = num % 1 
     res = ""
-    if w_part == 0:  # если целая часть 0 - 0 будет целой частью и в новой системе счисления
+    if w_part == 0:  
         res += str(0)
     else:
-        while w_part > 0:  # перевод челой части в новую систему счисления
+        while w_part > 0: 
             a = w_part % not2
             if a >= 10:
                 res += chr(87 + a)
@@ -56,8 +51,8 @@ def d_to_not2(num, not2):
     res = res[::-1]
     res += '.'
 
-    for i in range(10):  # точность вычисления дробной части (10 знаков после запятой в данном случае)
-        # перевод дробной части в новую систему счисления
+    for i in range(10): 
+        
         f_part = f_part * not2
         b = int(f_part)
         if b >= 10:
@@ -70,9 +65,9 @@ def d_to_not2(num, not2):
     return res
 
 
-number = input("Введите данное число: ").lower()  # Число, которое имеем на входе
-notation1 = int(input("Введите его основание: "))  # Система счисления имеющегося числа
-notation2 = int(input("Введите основание результата: "))  # Новая система счисления
-d_number = not1_to_d(number, notation1)  # Промежуточное десятичное число
-result = d_to_not2(d_number, notation2)  # Итоговое число
+number = input("Введите данное число: ").lower()  
+notation1 = int(input("Введите его основание: "))  
+notation2 = int(input("Введите основание результата: "))  
+d_number = not1_to_d(number, notation1)  
+result = d_to_not2(d_number, notation2)  
 print(result.upper())
